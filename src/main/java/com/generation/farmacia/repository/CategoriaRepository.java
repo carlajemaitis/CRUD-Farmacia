02b1f3310a6c94e153021686368b4c249cd211d1
Carla <carla.jemaitis@gmail.com>
2024-12-16 15:32:25 -0300

    6 métodos crud de categoria

--- src/main/java/com/generation/farmacia/repository/CategoriaRepository.java
+++ src/main/java/com/generation/farmacia/repository/CategoriaRepository.java
@@ -1,5 +1,16 @@
 package com.generation.farmacia.repository;
 
-public class CategoriaRepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.generation.farmacia.model.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+	
+	List<Categoria> findAllByNomeContainingIgnoreCase(@Param("nome") String nome);
 }
+
